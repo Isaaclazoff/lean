f5bf53235fe237a6d2e3a73a3a2c010084982a77
Isaac Lazoff <isaaclazoff@gmail.com>
2014-03-31 01:16:00 +0000

    First lean folder

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Lean::Application.config.secret_key_base = 'a445e2fea2a7de02dad9b0a291f15e0d3bf461dd2745bd5f8462990bb2903173b0755ed78f5ccc787dfaa6a47b1a1ba9bb4c0218139bfe146a7d439c86b76f99'
